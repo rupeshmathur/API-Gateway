b3e34a346b50990a128d2618bf97cc917310cbe0
Rupesh <mathurupesh92@gmail.com>
2025-11-20 23:27:04 +0530

    Fixed security config and application.yml

--- src/main/java/com/cloud/AWSDeployable/api/gateway/config/SecurityConfig.java
+++ src/main/java/com/cloud/AWSDeployable/api/gateway/config/SecurityConfig.java
@@ -1,21 +1,49 @@
 package com.cloud.AWSDeployable.api.gateway.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
+@Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+
         return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+
+                // VERY IMPORTANT
+                .cors().and()
+
                 .authorizeExchange(exchanges -> exchanges
-                        .pathMatchers("/api/notifications/**").permitAll()
+                        // Allow preflight requests
+                        .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyExchange().authenticated()
                 )
                 .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt)
                 .build();
+    }
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("http://localhost:3000");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsWebFilter(source);
     }
 }
